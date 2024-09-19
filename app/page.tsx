b24af4ec8cddf550cbebf883f70f90b406da7b1c
Nguyen Tien Thanh <thanhnthe163528@fpt.edu.vn>
2024-09-19 11:13:19 +0700

    feat: Add LogoTicker component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import LogoTicker from "@/components/LogoTicker";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <LogoTicker />
     </>
   );
 }
